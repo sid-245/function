b8d136412dcc3ee1ad098c89c6b99a0aac58e672
Siddhi@patil <118968670+sid-245@users.noreply.github.com>
2023-01-11 12:08:21 +0530

    Add files via upload

---
+++ func1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+   void sayhello();
+    sayhello();
+    sayhello();
+    
+   return 0;
+ }    
+  void sayhello()
+{
+  printf("\nhello friens!!!");
+  printf("\nwelcome to dypsn");
+
+}
